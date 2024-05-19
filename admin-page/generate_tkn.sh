04aab79ef63dc441e0154ea6bd448c4e3935cf60
exqzmepls <exqzmepls@yandex.ru>
2024-05-19 11:45:04 +0500

    Fix token splitting

--- admin-page/generate_tkn.sh
+++ admin-page/generate_tkn.sh
@@ -9,7 +9,7 @@ set -e
 readonly PLAIN_TOKEN=token
 readonly TOKEN_HASH=hash
 
-openssl rand -base64 60 >$PLAIN_TOKEN
+openssl rand -base64 60 | tr -d '\n' >$PLAIN_TOKEN
 
 # Using the OWASP minimum recommended settings
 argon2 "$(openssl rand -base64 32)" -e -id -k 19456 -t 2 -p 1 <$PLAIN_TOKEN >$TOKEN_HASH
